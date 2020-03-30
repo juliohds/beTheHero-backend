6c70e0954b1c06182d265de5f18f043b9c204a07
Julio Henrique <juliohenrique@Julios-MacBook-Air.local>
2020-03-29 22:25:51 -0300

    validation with celebrate

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+const { errors } = require('celebrate');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.listen(3333)
 app.use(routes);
+app.use(errors());
+app.listen(3333);
